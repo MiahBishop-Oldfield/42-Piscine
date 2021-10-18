f02394ed27b2f52d17b767ebfc4ab576275b897d
Miah Bishop-Oldfield <73050296+MiahBishop-Oldfield@users.noreply.github.com>
2021-10-18 17:13:14 +1030

    Add files via upload

---
+++ C05/ex06/ft_is_prime.c
@@ -0,0 +1,22 @@
+int	ft_is_prime(int nb)
+{
+	int	i;
+
+	i = 2;
+	if (nb <= 1)
+		return (0);
+	while (i < nb / 2)
+	{
+		if (nb % i == 0)
+			return (0);
+		i++;
+	}
+	return (1);
+}
+/*
+#include <stdio.h>
+int	main()
+{
+	printf("%d\n", ft_is_prime(4));
+}
+*/
